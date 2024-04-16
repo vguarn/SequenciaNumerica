bfdc727b2902e8e57d6e815443cbebdcee589208
Usuario <Usuario@CPD02-M14>
2024-04-16 11:45:32 -0300

    Adicionar arquivos de projeto.

---
+++ SequenciaNumerica/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenciaNumerica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i =1; i<=9; i++)
+            {
+                Console.WriteLine(i);
+                for (int j =1; j<=9; j++)
+                {
+                    if(i !=j)
+                    {
+                        Console.Write(j);
+                    }
+
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+      
+        }
+
+    }
+ 
